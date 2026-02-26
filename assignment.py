2fade535d846bbe8a3ba5bd2d2720decdcb9e2a6
keketoxopeus <keke.toxopeus@gmail.com>
2026-02-26 13:48:42 +0100

    Eerste probeersel tot visualiseren

--- assignment.py
+++ assignment.py
@@ -6,10 +6,35 @@
 #from worcgist.load_data import load_data
 #from worclipo.load_data import load_data
 #from worcliver.load_data import load_data
-#from hn.load_data import load_data
+from hn.load_data import load_data
 #from ecg.load_data import load_data
 
 data = load_data()
 print(f'The number of samples: {len(data.index)}')
 
 print(f'The number of columns: {len(data.columns)}')
+
+#%%
+import pandas as pd
+import numpy as np
+import os
+from glob import glob
+import matplotlib.pyplot as plt
+
+# Example: data should already exist
+# data = [[y1, x1], [y2, x2], ...]
+
+# Convert data to pandas DataFrame
+df = pd.DataFrame(data)
+
+# Select columns
+y = df.iloc[:, 0]   # first column → y-axis
+x = df.iloc[:, 1]   # second column → x-axis
+
+# Create scatter plot
+plt.figure()
+plt.scatter(x, y)
+plt.xlabel("Second column")
+plt.ylabel("First column")
+plt.title("Scatter plot of column 1 vs column 2")
+plt.show()
