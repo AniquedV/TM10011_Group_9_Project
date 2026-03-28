ff55eacbb2ef8bb24101de029788de7a2eb5c167
Bente Schimmel <b.f.schimmel@student.tudelft.nl>
2026-03-28 12:54:55 +0100

    commit

---
+++ Final_RandomForest.py
@@ -0,0 +1,201 @@
+#%%
+import sys
+print(sys.executable)
+#%% Data loading functions. Uncomment the one you want to use
+#from worcgist.load_data import load_data
+#from worclipo.load_data import load_data
+#from worcliver.load_data import load_data
+from hn.load_data import load_data
+#from ecg.load_data import load_data
+
+data = load_data()
+data = data.copy()  
+print(f'The number of samples: {len(data.index)}')
+
+print(f'The number of columns: {len(data.columns)}')
+
+#%% Imports
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from sklearn.preprocessing import RobustScaler
+from sklearn.decomposition import PCA
+from sklearn.model_selection import StratifiedKFold, GridSearchCV
+from scipy.cluster.hierarchy import linkage, dendrogram
+from sklearn.manifold import TSNE
+from sklearn.model_selection import train_test_split
+from scipy.stats import shapiro, normaltest, skew, kurtosis
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.preprocessing import LabelEncoder
+from sklearn import model_selection, metrics
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import learning_curve
+from sklearn.metrics import accuracy_score, recall_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve, auc
+from scipy.stats import randint
+from sklearn.model_selection import RandomizedSearchCV
+
+# Functions we will use
+def plot_learning_curve(estimator, title, X, y, axes, ylim=None, cv=None,
+                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
+
+    axes.set_title(title)
+    if ylim is not None:
+        axes.set_ylim(*ylim)
+    axes.set_xlabel("Training examples")
+    axes.set_ylabel("Score")
+
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes, scoring='roc_auc'
+    )
+
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+
+    axes.grid()
+    axes.fill_between(train_sizes,
+                      train_scores_mean - train_scores_std,
+                      train_scores_mean + train_scores_std,
+                      alpha=0.1)
+
+    axes.fill_between(train_sizes,
+                      test_scores_mean - test_scores_std,
+                      test_scores_mean + test_scores_std,
+                      alpha=0.1)
+
+    axes.plot(train_sizes, train_scores_mean, 'o-', label="Training AUC")
+    axes.plot(train_sizes, test_scores_mean, 'o-', label="Validation AUC")
+
+    axes.legend(loc="best")
+
+    return axes
+
+def plot_roc_curve(y_score, y_truth):
+    '''
+    Plot an ROC curve.
+    '''
+    # Only take scores for class = 1
+    y_score = y_score[:, 1]
+
+    fpr, tpr, _ = roc_curve(y_truth, y_score)
+    roc_auc = auc(fpr, tpr)
+
+    plt.figure(figsize=(7,5))
+    plt.plot(fpr, tpr,
+             lw=2, label='ROC curve (AUC = %0.3f)' % roc_auc)
+    plt.plot([0, 1], [0, 1],
+             lw=1, linestyle='--', label='Random Guess')
+
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curve (Random Forest)')
+    plt.legend(loc="lower right")
+    plt.grid(True)
+    plt.show()
+
+# Prepare data
+data['label_bin'] = LabelEncoder().fit_transform(data['label'])
+
+X = data.drop(columns=['label', 'label_bin'])
+y = data['label_bin']
+
+# pre-processing
+def preprocess_data(X):
+    X = X.copy()
+
+    Q1 = X.quantile(0.25)
+    Q3 = X.quantile(0.75)
+    IQR = Q3 - Q1
+
+    outliers = (X < (Q1 - 1.5 * IQR)) | (X > (Q3 + 1.5 * IQR))
+    X = X.mask(outliers, np.nan)
+
+    X = X.fillna(X.median())
+
+    return X
+
+X_cleaned = preprocess_data(X)
+
+scaler = RobustScaler()
+X2 = scaler.fit_transform(X_cleaned)
+y2 = y.values
+
+# Train/test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X2, y2, test_size=0.2, random_state=42, stratify=y2
+)
+
+# Define model
+model = RandomForestClassifier(random_state=42)
+
+# Define hyperparameter distributions for RandomizedSearch
+param_distributions = {
+    "n_estimators": randint(50, 150),         #number of trees
+    "max_depth": randint(2, 5),              #max depth of trees
+    "min_samples_split": randint(2, 20),      #min samples to split
+    "min_samples_leaf": randint(5, 10),       #min samples per leaf
+}
+
+# Stratified CV
+cv_inner = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+#%% RandomizedSearchCV
+random_search = RandomizedSearchCV(
+    estimator=model,
+    param_distributions=param_distributions,
+    n_iter=20,                  # amount of random iterations
+    scoring='roc_auc',
+    cv=cv_inner,
+    random_state=42,
+    n_jobs=-1
+)
+
+# Fit RandomizedSearchCV on training data
+random_search.fit(X_train, y_train)
+
+# Get the best model
+best_model = random_search.best_estimator_
+print("Best parameters:", random_search.best_params_)
+
+#%% Learning curve 
+fig, ax = plt.subplots(figsize=(7,5))
+
+plot_learning_curve(
+    estimator=best_model,
+    title="Learning Curve (Random Forest)",
+    X=X_train,
+    y=y_train,
+    axes=ax,
+    cv=5,
+    n_jobs=-1,
+    train_sizes=np.linspace(0.1, 1.0, 10)
+)
+
+plt.grid()
+plt.show()
+#%% Final evaluation on test set
+y_test_probs = best_model.predict_proba(X_test)
+auc_test = roc_auc_score(y_test, y_test_probs[:, 1])
+
+print("Final Test AUC:", auc_test)
+
+# Performance scores
+y_test_pred = best_model.predict(X_test)
+tn, fp, fn, tp = confusion_matrix(y_test, y_test_pred).ravel()
+accuracy = accuracy_score(y_test, y_test_pred)
+sensitivity = recall_score(y_test, y_test_pred)  # same as tp / (tp+fn)
+specificity = tn / (tn + fp)
+print("accuracy:", accuracy)
+print("sensitivity:", sensitivity)
+print("specificity:", specificity)
+
+# ROC curve
+plot_roc_curve(y_test_probs, y_test)
---
+++ Final_SVM.py
@@ -0,0 +1,418 @@
+
+# ## Data loading and cleaning
+# Below are functions to load the dataset of your choice. After that, it is all up to you to create and evaluate a classification method. Beware, there may be missing values in these datasets. Good luck!
+
+
+#%% Data loading functions. Uncomment the one you want to use
+#from worcgist.load_data import load_data
+#from worclipo.load_data import load_data
+#from worcliver.load_data import load_data
+from hn.load_data import load_data
+#from ecg.load_data import load_data
+
+data = load_data()
+
+print(f'The number of samples: {len(data.index)}')
+print(f'The number of columns: {len(data.columns)}')
+
+
+#%% Imports
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from sklearn.preprocessing import RobustScaler
+from sklearn.decomposition import PCA
+from scipy.cluster.hierarchy import linkage, dendrogram
+from sklearn.manifold import TSNE
+from sklearn.model_selection import train_test_split
+from scipy.stats import shapiro, normaltest, skew, kurtosis
+from sklearn.svm import SVC
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import GridSearchCV
+from sklearn import model_selection, metrics
+import seaborn
+
+# Some functions we will use
+
+# Construct classifiers
+svmlin = SVC(kernel='linear', gamma='scale', probability=True)
+svmrbf = SVC(kernel='rbf', gamma='scale', probability=True)
+svmpoly = SVC(kernel='poly', degree=3, gamma='scale', probability=True)
+
+clsfs = [
+    ("SVM linear", svmlin),
+    ("SVM poly", svmpoly),
+    ("SVM rbf", svmrbf)
+]
+
+def colorplot(clf, ax, x, y, h=100):
+    '''
+    Overlay the decision areas as colors in an axes.
+
+    Input:
+        clf: trained classifier
+        ax: axis to overlay color mesh on
+        x: feature on x-axis
+        y: feature on y-axis
+        h(optional): steps in the mesh
+    '''
+    # Create a meshgrid the size of the axis
+    xstep = (x.max() - x.min() ) / 20.0
+    ystep = (y.max() - y.min() ) / 20.0
+    x_min, x_max = x.min() - xstep, x.max() + xstep
+    y_min, y_max = y.min() - ystep, y.max() + ystep
+    h = max((x_max - x_min, y_max - y_min))/h
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, x_max]x[y_min, y_max].
+    if hasattr(clf, "decision_function"):
+        Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+    else:
+        Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])
+    if len(Z.shape) > 1:
+        Z = Z[:, 1]
+
+    # Put the result into a color plot
+    cm = plt.cm.RdBu_r
+    Z = Z.reshape(xx.shape)
+    ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+    del xx, yy, x_min, x_max, y_min, y_max, Z, cm
+
+#%% visualisatie
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import RobustScaler
+from sklearn.model_selection import train_test_split
+
+
+data['label_bin'] = LabelEncoder().fit_transform(data['label'])
+print(data[['label', 'label_bin']].head())
+data['label_bin'] = LabelEncoder().fit_transform(data['label'])
+print(data[['label', 'label_bin']].head())
+
+X = data.drop(columns=['label', 'label_bin'])
+y = data['label_bin']
+
+def preprocess_data(X):
+    X = X.copy()
+
+    Q1 = X.quantile(0.25)
+    Q3 = X.quantile(0.75)
+    IQR = Q3 - Q1
+
+    outliers = (X < (Q1 - 1.5 * IQR)) | (X > (Q3 + 1.5 * IQR))
+    X = X.mask(outliers, np.nan)
+
+    X = X.fillna(X.median())
+
+    return X
+
+X_cleaned = preprocess_data(X)
+
+
+# Train/test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X_cleaned, y, test_size=0.2, random_state=42
+)
+
+
+# Scale features
+scaler = RobustScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+print(X_cleaned.shape, y.shape)
+
+
+#%%
+
+#pca needed for visualization, needs 2 components, so pca to reducxe to 2 dimensions, should be ran individually from rest of code, next part wont run after this, as its then pca
+pca = PCA(n_components=2)
+X_train_2D = pca.fit_transform(X_train_scaled)
+
+Xs = [X_train_2D]
+Ys = [y_train.values]
+
+
+# Construct classifiers
+degrees = [1, 3, 5]
+coef0s = [0.01, 0.5, 1]
+slacks = [0.01, 0.5, 1]
+
+clsfs = list()
+for degree in degrees:
+    for coef0 in coef0s:
+        for slack in slacks:
+            clsfs.append(SVC(kernel='poly', degree=degree, coef0=coef0, C=slack, gamma='scale'))
+
+# First make plot without classifiers:
+num = 0
+fig = plt.figure(figsize=(24,8*len(clsfs)))
+for X, Y in zip(Xs, Ys):
+    ax = fig.add_subplot(len(clsfs) + 1, 3, num + 1)
+    ax.scatter(X[:, 0], X[:, 1], marker='o', c=Y,
+        s=25, edgecolor='k', cmap=plt.cm.Paired)
+    num += 1
+
+# Now use the classifiers on all datasets
+for clf in clsfs:
+    for X, Y in zip(Xs, Ys):
+        clf.fit(X, Y)
+        ax = fig.add_subplot(len(clsfs) + 1, 3, num + 1)
+        ax.scatter(X[:, 0], X[:, 1], marker='o', c=Y,
+            s=25, edgecolor='k', cmap=plt.cm.Paired)
+        colorplot(clf, ax, X[:, 0], X[:, 1])
+        y_pred = clf.predict(X)
+        t = f"degree: {clf.degree}, coef0: {clf.coef0}, C: {clf.C}. "
+        t = t + ("Misclassified: %d / %d" % ((Y != y_pred).sum(), X.shape[0]))
+        ax.set_title(t)
+        num += 1
+
+# Note: you may get a FutureWarning, which you can for now just ignore
+print(X.shape, y.shape)
+#%% crossvalidatie, nu nog knn
+                  
+print(X.shape, y.shape)
+#X2 en y2 maken
+X_scaled = scaler.fit_transform(X)
+y_array = y.values
+print(X_scaled.shape, y_array.shape)
+
+# Create a 10 fold stratified CV iterator, of 20? voor nu 10
+cv_10fold = model_selection.StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
+results = []
+
+param_grid = {"n_neighbors": list(range(1,26,2))}
+
+
+models_and_grids = [
+    ("linear", SVC(probability=True), {
+        "kernel": ["linear"],
+        "C": [0.1, 1, 10]
+    }),
+    ("rbf", SVC(probability=True), {
+        "kernel": ["rbf"],
+        "C": [0.1, 1, 10],
+        "gamma": ["scale", 0.01, 0.1]
+    }),
+    ("poly", SVC(probability=True), {
+        "kernel": ["poly"],
+        "C": [0.1, 1, 10],
+        "degree": [2, 3, 5],
+        "coef0": [0, 1]
+    })
+]
+
+best_models = []
+
+for name, model, param_grid in models_and_grids:
+    print(f"\n{name} SVM - tuning")
+
+    grid = GridSearchCV(
+        estimator=model,
+        param_grid=param_grid,
+        cv=cv_10fold,
+        scoring="roc_auc",
+        n_jobs=-1,
+        verbose=1
+    )
+
+    grid.fit(X_scaled, y_array)
+
+    print("Best parameters:", grid.best_params_)
+    best_model = grid.best_estimator_
+    best_models.append((name, best_model))
+
+    results = []
+    for train_index, test_index in cv_10fold.split(X_scaled, y_array):
+        X_cv_train, X_cv_test = X_scaled[train_index], X_scaled[test_index]
+        y_cv_train, y_cv_test = y_array[train_index], y_array[test_index]
+
+        best_model.fit(X_cv_train, y_cv_train)
+
+        y_probs = best_model.predict_proba(X_cv_test)[:,1]
+        auc = metrics.roc_auc_score(y_cv_test, y_probs)
+        results.append({'auc': auc, 'set': 'test'})
+
+        y_train_probs = best_model.predict_proba(X_cv_train)[:,1]
+        auc_train = metrics.roc_auc_score(y_cv_train, y_train_probs)
+        results.append({'auc': auc_train, 'set': 'train'})
+
+    results_df = pd.DataFrame(results)
+    sns.boxplot(y='auc', x='set', data=results_df)
+    plt.title(f"Tuned {name} SVM")
+    plt.show()
+
+#%% learning curve en roc curve 
+from sklearn.model_selection import learning_curve
+from sklearn.metrics import roc_curve, auc
+
+#fill in hyperparameters found with gridsearchcv
+clsfs = [
+    ("SVM linear", SVC(kernel='linear', C=0.1, gamma='scale', probability=True)),
+    ("SVM poly",   SVC(kernel='poly', C=10, coef0=1, degree=2, gamma='scale', probability=True)),
+    ("SVM rbf",    SVC(kernel='rbf', C=10, gamma='scale', probability=True))
+]
+
+print(clsfs)
+def plot_learning_curve(estimator, title, X, y, axes, ylim=None, cv=None,
+                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
+    """
+    Generate 3 plots: the test and training learning curve, the training
+    samples vs fit times curve, the fit times vs score curve.
+
+    Parameters
+    ----------
+    estimator : object type that implements the "fit" and "predict" methods
+        An object of that type which is cloned for each validation.
+
+    title : string
+        Title for the chart.
+
+    X : array-like, shape (n_samples, n_features)
+        Training vector, where n_samples is the number of samples and
+        n_features is the number of features.
+
+    y : array-like, shape (n_samples) or (n_samples, n_features), optional
+        Target relative to X for classification or regression;
+        None for unsupervised learning.
+
+    axes : array of 3 axes, optional (default=None)
+        Axes to use for plotting the curves.
+
+    ylim : tuple, shape (ymin, ymax), optional
+        Defines minimum and maximum yvalues plotted.
+
+    cv : int, cross-validation generator or an iterable, optional
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+          - None, to use the default 5-fold cross-validation,
+          - integer, to specify the number of folds.
+          - :term:`CV splitter`,
+          - An iterable yielding (train, test) splits as arrays of indices.
+
+        For integer/None inputs, if ``y`` is binary or multiclass,
+        :class:`StratifiedKFold` used. If the estimator is not a classifier
+        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
+
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validators that can be used here.
+
+    n_jobs : int or None, optional (default=None)
+        Number of jobs to run in parallel.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+        for more details.
+
+    train_sizes : array-like, shape (n_ticks,), dtype float or int
+        Relative or absolute numbers of training examples that will be used to
+        generate the learning curve. If the dtype is float, it is regarded as a
+        fraction of the maximum size of the training set (that is determined
+        by the selected validation method), i.e. it has to be within (0, 1].
+        Otherwise it is interpreted as absolute sizes of the training sets.
+        Note that for classification the number of samples usually have to
+        be big enough to contain at least one sample from each class.
+        (default: np.linspace(0.1, 1.0, 5))
+    """
+
+    axes.set_title(title)
+    if ylim is not None:
+        axes.set_ylim(*ylim)
+    axes.set_xlabel("Training examples")
+    axes.set_ylabel("Score")
+
+    train_sizes, train_scores, test_scores  = \
+        learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs,
+                       train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+
+    # Plot learning curve
+    axes.grid()
+    axes.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                         train_scores_mean + train_scores_std, alpha=0.1,
+                         color="r")
+    axes.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1,
+                         color="g")
+    axes.plot(train_sizes, train_scores_mean, 'o-', color="r",
+                 label="Training score")
+    axes.plot(train_sizes, test_scores_mean, 'o-', color="g",
+                 label="Cross-validation score")
+    axes.legend(loc="best")
+
+    return plt
+
+def plot_roc_curve(y_score, y_truth):
+    '''
+    Plot an ROC curve.
+    '''
+    # Only take scores for class = 1
+    y_score = y_score[:, 1]
+
+    # Compute ROC curve and ROC area for each class
+    fpr, tpr, _ = roc_curve(y_truth, y_score)
+    roc_auc = auc(fpr, tpr)
+
+    # Plot the ROC curve
+    plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.show()
+
+for name, clf in clsfs:
+    clf.fit(X_train_scaled, y_train)
+
+    if hasattr(clf, "predict_proba"):
+        y_score = clf.predict_proba(X_test_scaled)
+    else:
+        # For SVM without probability (just in case)
+        scores = clf.decision_function(X_test_scaled)
+        y_score = np.vstack([1 - scores, scores]).T
+
+    print(f"ROC curve for {name}")
+    plot_roc_curve(y_score, y_test)
+
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(figsize=(8,6))
+plot_learning_curve(svmlin, "Learning Curve: SVM Linear", X_train_scaled, y_train, ax, cv=10)
+plt.show()
+
+fig, ax = plt.subplots(figsize=(8,6))
+plot_learning_curve(svmpoly, "Learning Curve: SVM Poly", X_train_scaled, y_train, ax, cv=10)
+plt.show()
+
+fig, ax = plt.subplots(figsize=(8,6))
+plot_learning_curve(svmrbf, "Learning Curve: SVM RBF", X_train_scaled, y_train, ax, cv=10)
+plt.show()
+
+from sklearn.metrics import confusion_matrix, accuracy_score, recall_score
+
+for name, clf in clsfs:
+    clf.fit(X_train_scaled, y_train)
+
+    y_test_pred = clf.predict(X_test_scaled)
+    y_test_probs = clf.predict_proba(X_test_scaled)
+
+    tn, fp, fn, tp = confusion_matrix(y_test, y_test_pred).ravel()
+
+    print(f"\n{name}")
+    print("accuracy:", accuracy_score(y_test, y_test_pred))
+    print("sensitivity:", recall_score(y_test, y_test_pred))
+    print("specificity:", tn / (tn + fp))
+
+    plot_roc_curve(y_test_probs, y_test)
